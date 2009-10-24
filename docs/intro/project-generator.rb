6ebaafc58569de5326d4e081e8cfa4bc87b81d6b
Bob Hutchison <hutch@recursive.ca>
2009-10-24 12:03:54 -0400

    ...

--- docs/intro/project-generator.rb
+++ docs/intro/project-generator.rb
@@ -1,5 +1,25 @@
 class ProjectGenerator
 
+#  def directory
+#    File.join(%w{ . xampl_generated_code })
+#  end
+#
+#  def filenames
+#    Dir.glob("./xml/**/*.xml")
+#  end
+#
+#  def print
+#    nil
+#  end
+#
+#  def persisted_attributes
+#    %w{ pid }
+#  end
+#
+#  def indexed_attributes
+#    %w{ id }
+#  end
+
   def resolve_namespaces
     # any array of arrays
     # each sub-array:
@@ -7,6 +27,8 @@ class ProjectGenerator
     #       an empty string is the default namespace
     #    1: a ruby Module name (get the cases right)
     #    2: a namespace prefix used when writing xml, optional. A generated prefix will be used otherwise.
+
+    #[]
 
     [
             [ '', 'Example1']
